acfb989f12ca0f5ccdff565b81f77cf30c870d00
Samantha Brown <S.BrownEMS@gmail.com>
2022-12-21 15:11:57 -0500

    Created /observations route

--- src/observations/ObservationCreate.js
+++ src/observations/ObservationCreate.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { createObservation } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
 
 
 function ObservationCreate(){
     const history = useHistory();
+    const [error, setError] = useState(null);
 
     const [observation, setObservation] = useState({
         latitute: "",
@@ -18,14 +20,24 @@ function ObservationCreate(){
 
     function submitHandler(event){
         event.preventDefault();
-        createObservation(observation).then(() => {
-            history.push("/");
-        });
+        createObservation(observation)
+            .then(()=> {
+                history.push("/");
+            })
+            .catch(setError);
     };
+
+    function changeHandler({ target: {name, value} }) {
+        setObservation((previousObservation) => ({
+            ...previousObservation,
+            [name]: value,
+        }))
+    }
 
     return(
         <main>
             <h1 className="mb-3">Create Observation</h1>
+            <ErrorAlert error={error} />
             <form onSubmit={submitHandler} className="mb-4">
                 <div className="row mb-3">
                     <div className="col-6 form-group">
@@ -43,49 +55,52 @@ function ObservationCreate(){
                             onChange={changeHandler}
                             required={true}
                         />    
+
                         <small className="form-text text-muted">Enter a value between -90 and 90.</small>
-                        +          </div>
-                        +          <div className="col-6">
-                        +            <label className="form-label" htmlFor="longitude">
-                        +              Longitude
-                        +            </label>
-                        +            <input
-                        +              className="form-control"
-                        +              id="longitude"
-                        +              name="longitude"
-                        +              type="number"
-                        +              max="180"
-                        +              min="-180"
-                        +              value={observation.longitude}
-                        +              onChange={changeHandler}
-                        +              required={true}
-                        +            />
-                        +            <small className="form-text text-muted">Enter a value between -180 and 180.</small>
-                        +          </div>
-                        +        </div>
-                        +        <div className="mb-3">
-                        +          <label className="form-label" htmlFor="cloudCover">
-                        +            Sky conditions
-                        +          </label>
-                        +          <select
-                        +            className="form-control"
-                        +            id="sky_condition"
-                        +            name="sky_condition"
-                        +            value={observation.sky_condition}
-                        +            onChange={changeHandler}
-                        +            required={true}
-                        +          >
-                        +            <option value="">Select a sky condition option</option>
-                        +            <option value="100">Cloudless</option>
-                        +            <option value="101">Some clouds</option>
-                        +            <option value="102">Cloud covered</option>
-                        +            <option value="103">Foggy</option>
-                        +            <option value="104">Raining</option>
-                        +            <option value="106">Snowing</option>
-                        +            <option value="108">Hailing</option>
-                        +            <option value="109">Thunderstorms</option>
-                        +          </select>
-                        +        </div>   
+                    </div>
+
+                    <div className="col-6">
+                        <label className="form-label" htmlFor="longitude">
+                           Longitude
+                        </label>
+                        <input
+                          className="form-control"
+                          id="longitude"
+                          name="longitude"
+                          type="number"
+                          max="180"
+                          min="-180"
+                          value={observation.longitude}
+                          onChange={changeHandler}
+                          required={true}
+                        />
+                         <small className="form-text text-muted">Enter a value between -180 and 180.</small>
+                    </div>
+                </div>
+                           
+                        <div className="mb-3">
+                                 <label className="form-label" htmlFor="cloudCover">
+                                   Sky conditions
+                                 </label>
+                              <select
+                                    id="sky_condition"
+                                    className="form-control"
+                                    name="sky_condition"
+                                    value={observation.sky_condition}
+                                    onChange={changeHandler}
+                                    required={true}
+                                  >
+                                    <option value="">Select a sky condition option</option>
+                                    <option value="100">Cloudless</option>
+                                    <option value="101">Some clouds</option>
+                                    <option value="102">Cloud covered</option>
+                                    <option value="103">Foggy</option>
+                                    <option value="104">Raining</option>
+                                    <option value="106">Snowing</option>
+                                    <option value="108">Hailing</option>
+                                    <option value="109">Thunderstorms</option>
+                                  </select>
+                                </div>   
                 <div>
                     <button
                         type="button"
